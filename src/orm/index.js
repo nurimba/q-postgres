5381ccb24023815e349cba2c2e7ce03cc06e5e8e
Luiz Amorim <luiz.simples@gmail.com>
2016-09-09 16:15:07 -0300

    Added delete data in ORM

--- src/orm/index.js
+++ src/orm/index.js
@@ -1,11 +1,13 @@
 import findById from 'orm/findById'
 import selectData from 'orm/select'
+import deleteData from 'orm/delete'
 import {insertData, updateData} from 'orm/insUpd'
 
 const ormModel = (tables, connection, modelName) => {
   const schema = JSON.parse(JSON.stringify(tables[modelName]))
   const model = {schema}
   model.select = selectData.bind(this, connection, schema)
+  model.delete = deleteData.bind(this, connection, schema)
   model.insert = insertData.bind(this, tables, connection, schema)
   model.update = updateData.bind(this, tables, connection, schema)
   model.findById = findById.bind(this, tables, connection, schema)
