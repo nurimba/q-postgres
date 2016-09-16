0ed2caa27169db78d089faba2382217b03bee391
Luiz Amorim <luiz.simples@gmail.com>
2016-09-16 18:02:34 -0300

    Added cast money, interger, percent and reference.

--- src/orm/objRow.js
+++ src/orm/objRow.js
@@ -1,4 +1,4 @@
-import {DATE} from 'gen/types'
+import {DATE, MONEY, PERCENT, INTEGER, REFERENCES} from 'gen/types'
 
 export const formatDate = (value) => {
   if (!value || !String(value).trim()) return null
@@ -8,12 +8,24 @@ export const formatDate = (value) => {
   return `${dt.getFullYear()}-${mon}-${day}`
 }
 
+export const formatFloat = (value) => {
+  if (!value || !String(value).trim()) return null
+  return parseFloat(value)
+}
+
+export const formatInteger = (value) => {
+  if (!value || !String(value).trim()) return null
+  return parseInt(value, 10)
+}
+
 export default (schema, row) => {
   const data = {}
 
   Object.keys(schema.fields).forEach(field => {
     let value = row[field.toLowerCase()]
     if (schema.fields[field] === DATE) value = formatDate(value)
+    if ([MONEY, PERCENT].indexOf(schema.fields[field]) > -1) value = formatFloat(value)
+    if ([INTEGER, REFERENCES].indexOf(schema.fields[field]) > -1) value = formatInteger(value)
     Object.assign(data, {[field]: value})
   })
 
--- webpack.prod.js
+++ webpack.prod.js
@@ -3,7 +3,7 @@ const webpack = require('webpack')
 const nodeExternals = require('webpack-node-externals')
 
 module.exports = {
-  devtool: '#source-map',
+  devtool: 'cheap-module-source-map',
   debug: false,
 
   target: 'node', // in order to ignore built-in modules like path, fs, etc.
