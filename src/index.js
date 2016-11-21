13dd61baa58dc5a1bf8f5fe2e930e710b74fb9a3
Luiz Amorim <luiz.simples@gmail.com>
2016-11-21 17:26:13 -0200

    Remove comments from build.

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import {Pool} from 'pg'
+import pg from 'pg'
 import orm from 'orm'
 import database from 'db'
 import typeSchemas from 'gen/types'
@@ -8,7 +8,7 @@ export const types = typeSchemas
 export const genSQL = generatorsSQL
 
 export default (config) => {
-  const pool = new Pool(config)
+  const pool = new pg.Pool(config)
   const connect = database.bind(this, pool, config)
   return {connect, orm, pool}
 }
--- webpack.prod.js
+++ webpack.prod.js
@@ -33,19 +33,11 @@ module.exports = {
   },
 
   plugins: [
-    new webpack.optimize.OccurenceOrderPlugin(),
+    new webpack.optimize.OccurrenceOrderPlugin(),
     new webpack.optimize.DedupePlugin(),
-
-    new webpack.DefinePlugin({
-      'process.env': {
-        NODE_ENV: JSON.stringify('production')
-      }
-    }),
-
     new webpack.optimize.UglifyJsPlugin({
-      compress: {
-        warnings: false
-      }
+      compress: { warnings: false },
+      output: { comments: false }
     })
   ]
 }
